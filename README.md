9a5e70ff4973a7d0cb00c2ff5fd53db3975504da
ANILSRGT <anilsorgit@outlook.com.tr>
2025-01-20 13:57:00 +0300

    Add Firebase integration and update README with setup instructions

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# penta_story
+# Penta Story
 
-A new Flutter project.
+Penta Story is a story book app. Users can read stories and listen to them.
+
+## Setup
+
+#### Clone the repository
+
+```bash
+git clone https://github.com/ANILSRGT/flutter_penta_story.git
+```
+
+#### Install dependencies
+
+```bash
+flutter pub get
+```
+
+#### [Firebase](https://console.firebase.google.com/) setup
+
+1.  Create a new project in Firebase
+2.  Add an Android app (com.pentaio_example.penta_story) to the project with FirebaseCLI
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:penta_story/core/configs/enums/app_localiaztions_enum.dart';
 import 'package:penta_story/core/configs/router/app_router.dart';
 import 'package:penta_story/core/localization/localization_manager.dart';
 import 'package:penta_story/core/providers/theme_notifier.dart';
+import 'package:penta_story/firebase_options.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
 
   runApp(
